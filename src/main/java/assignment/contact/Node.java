b8d16df124d97800160c0c5609d0d152ad0f5e8c
Mohanraj1100 <123471382+Mohanraj1100@users.noreply.github.com>
2023-02-12 10:56:51 +0530

    Update Node.java

--- src/main/java/assignment/contact/Node.java
+++ src/main/java/assignment/contact/Node.java
@@ -2,10 +2,10 @@ package assignment.contact;
 
 public class Node {
 	String contactname;
-	double phonenumber;
+	long phonenumber;
 	String emailaddress;
 	Node next;
-	Node(String contactname,double phonenumber,String emailaddress)
+	Node(String contactname,long phonenumber,String emailaddress)
 	{
 		this.contactname=contactname;
 		this.phonenumber=phonenumber;
